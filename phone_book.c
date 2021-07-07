8d043dc3f9a0108e07033f129eb2f9ef80893eb2
Tarun-Teja18 <84650993+Tarun-Teja18@users.noreply.github.com>
2021-07-07 16:28:27 +0530

    Update phone_book.c
    first attempt

--- phone_book.c
+++ phone_book.c
@@ -62,7 +62,20 @@ int main(int argc, char *argv[]) {
     fclose(fp);
     exit(0);
   } else if (strcmp(argv[1], "search") == 0) {  /* Handle search */
-    printf("NOT IMPLEMENTED!\n"); /* TBD  */
+    if(argc != 3) {
+    	print_usage("Improper arguments for search",argv[0]);
+    	exit(1);
+    }
+    FILE *fp = open_db_file();
+    char* name = argv[2];
+    /* TBD  */
+    if (!search(fp, name)) {
+      printf("no match\n");
+      fclose(fp);
+      exit(1);
+    }
+    fclose(fp);
+    exit(0);
   } else if (strcmp(argv[1], "delete") == 0) {  /* Handle delete */
     if (argc != 3) {
       print_usage("Improper arguments for delete", argv[0]);
@@ -93,8 +106,14 @@ FILE *open_db_file() {
 }
   
 void free_entries(entry *p) {
-  /* TBD */
-  printf("Memory is not being freed. This needs to be fixed!\n");  
+  //free(p->name);
+  //free(p->phone);
+	/* TBD */
+	while(p!=NULL) {
+		free(p);
+		p = p -> next;
+	}
+  //printf("Memory is not being freed. This needs to be fixed!\n");  
 }
 
 void print_usage(char *message, char *progname) {
@@ -178,14 +197,31 @@ void add(char *name, char *phone) {
 void list(FILE *db_file) {
   entry *p = load_entries(db_file);
   entry *base = p;
+  int count=0;
   while (p!=NULL) {
     printf("%-20s : %10s\n", p->name, p->phone);
     p=p->next;
+    count++;
   }
-  /* TBD print total count */
+  printf("Total entries : %d\n",count);
   free_entries(base);
 }
 
+int search(FILE *db_file,char *name) {
+	entry *p = load_entries(db_file);
+	entry *base = p;
+	int check = 0;
+	while (p!=NULL) {
+		if(strcmp(p->name, name) == 0) {
+			printf("%s\n", p->phone);
+			check = 1;
+			break;
+		}
+		p = p->next;
+	}
+	free_entries(base);
+	return check;
+}
 
 int delete(FILE *db_file, char *name) {
   entry *p = load_entries(db_file);
@@ -207,6 +243,19 @@ int delete(FILE *db_file, char *name) {
       */
 
       /* TBD */
+      if(strcmp(base->name, name) == 0) {
+      	base = p->next;
+      }
+      else {
+      	p = p->next;
+      	prev->next = p;
+      }
+      deleted = 1;
+      break;
+    }
+    else {
+    	prev = p;
+    	p = p->next;
     }
   }
   write_all_entries(base);
